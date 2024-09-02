d2a054a61cc2d32eb31c61fc79bddb6af84ec683
Latif Altay <latifaltay00@gmail.com>
2024-09-02 17:34:32 +0300

    Handlers and commands are created

---
+++ CQRSDesignPattern/DesignPattern.CQRS/CQRSPattern/Handlers/GetProductQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using DesignPattern.CQRS.CQRSPattern.Results;
+using DesignPattern.CQRS.DAL;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace DesignPattern.CQRS.CQRSPattern.Handlers
+{
+    public class GetProductQueryHandler
+    {
+        private readonly Context _context;
+
+        public GetProductQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+
+        public List<GetProductQueryResult> Handle() 
+        {
+            var values = _context.Products.Select(x => new GetProductQueryResult 
+            {
+                ProductId = x.ProductId,
+                ProductName = x.ProductName,
+                Stock = x.Stock,
+                Description = x.Description,
+                Price = x.Price,
+                Status = x.Status
+            }).ToList();
+
+            return values;
+        }
+
+    }
+}
